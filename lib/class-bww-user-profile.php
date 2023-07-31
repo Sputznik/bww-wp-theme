c83c52b99ad7154fd7e19fb41f7aa829377747ac
psychome004 <anil.stephen66@gmail.com>
2023-07-31 17:01:13 +0530

    Option to add custom user-roles

--- lib/class-bww-user-profile.php
+++ lib/class-bww-user-profile.php
@@ -5,6 +5,7 @@ class BWW_USER_PROFILE {
   function __construct() {
     add_filter( 'manage_users_columns', array( $this, 'manage_users_columns' ) ); // ADD CUSTOM COLUMN TO USER LIST TABLE
     add_action( 'manage_users_custom_column', array( $this, 'manage_users_custom_column' ), 10, 3 ); // ADD CONTENT TO CUSTOM COLUMN
+    add_action( 'wp_ajax_bww_add_user_roles', array( $this, 'add_user_roles' ) ); // ADD CUSTOM USER ROLES
   }
 
   function manage_users_columns( $columns ){
@@ -15,6 +16,33 @@ class BWW_USER_PROFILE {
   function manage_users_custom_column( $value, $column_name, $user_id ){
     if ( 'user_id' === $column_name ){ return $user_id; }
     return $value;
+  }
+
+  function add_user_roles(){
+    $capabilities = get_role( 'author' )->capabilities; // GET AUTHOR CAPABILITIES
+
+    $roles = array(
+      'bww_speaker'   => 'Speaker',
+      'bww_moderator' => 'Moderator'
+    );
+
+    if( $capabilities && count( $capabilities ) > 0 ){
+      foreach ( $roles as $role => $name ){
+        // CHECK IF ROLE ALREADY EXISTS
+        if( !$GLOBALS['wp_roles']->is_role( $role ) ){
+          $is_role_added = add_role( $role, $name, $capabilities ); // ADDS A ROLE IF IT DOES NOT EXIST
+
+          if( $is_role_added ){
+            echo "Role: <strong>$name</strong> has been created <br/>";
+          } else{
+            echo "Role: <strong>$name</strong> could not be created <br/>";
+          }
+        } else {
+          echo "Role: <strong>$name</strong> has been already created <br/>";
+        }
+      }
+    }
+    wp_die();
   }
 
 }
