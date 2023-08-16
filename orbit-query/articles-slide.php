3cd06065999873b23d5762485c62e2b84f709a14
psychome004 <anil.stephen66@gmail.com>
2023-08-16 18:12:36 +0530

    Update orbit slide template

--- orbit-query/articles-slide.php
+++ orbit-query/articles-slide.php
@@ -1,9 +1,11 @@
 <div id="<?php _e( $atts['id'] );?>" data-target="<?php _e('div.bww-slide-body');?>" data-url="<?php _e( $atts['url'] );?>" class="articles-bww-slide">
   <?php while( $this->query->have_posts() ) : $this->query->the_post(); ?>
     <?php
+      $current_post   = $this->query->post;
       $permalink      = get_the_permalink();
-      $thumbnail      = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' )[0];
+      $thumbnail      = wp_get_attachment_image_src( get_post_thumbnail_id( $current_post->ID ), 'full' )[0];
       $background_img = !empty( $thumbnail ) ? 'style="background-image:url('.$thumbnail.');"' : "";
+      $slide_headline = $current_post->post_status == 'future' ? 'UPCOMING' : 'RECENT';
     ?>
     <div class="bww-slide-body">
       <div class="orbit-thumbnail-bg" <?php _e( $background_img ); ?>>
@@ -11,7 +13,7 @@
       </div>
       <div class="post-desc">
         <div class="bww-headline-wrapper">
-          <span class="slide-headline">UPCOMING WEBINAR</span>
+          <span class="slide-headline"><?php _e( $slide_headline );?> WEBINAR</span>
         </div>
         <h3 class="post-title"><a href="<?php _e( $permalink );?>"><?php the_title();?></a></h3>
         <p class="meta"><strong><?php the_time( 'F j, Y' );?></strong></p>
