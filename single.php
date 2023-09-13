dda32567e558808ada138ed130d6c2d3f3c1c0bb
psychome004 <anil.stephen66@gmail.com>
2023-09-13 16:04:55 +0530

    Update post datetime

--- single.php
+++ single.php
@@ -3,6 +3,7 @@
  * The template for displaying all single posts
  */
 get_header();
+$post_date = get_the_time( 'M j, Y' );
 ?>
 <div id="bww-single-post">
   <?php if( have_posts() ): while( have_posts() ): the_post(); ?>
@@ -12,7 +13,12 @@ get_header();
         <div class="row">
           <div class="col-sm-12">
             <h1 class="post-title"><?php the_title();?></h1>
-            <p class="post-date"><?php the_time( 'M j, Y' );?> | <?php the_time('g:i A'); ?></p>
+            <p class="post-date">
+              <?php
+                if( $post->post_status == 'publish' ){ _e( $post_date ); }
+                else{ _e( $post_date ); ?> | <?php the_time('g:i A T'); }
+              ?>
+            </p>
             <?php get_template_part( 'partials/post/bww-register-btn' ); ?>
             <div class="post-content <?php _e( "post-".$post->post_status )?>"><?php the_content(); ?></div>
             <?php get_template_part( 'partials/post/bww-register-btn' ); ?>
